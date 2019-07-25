32814d1f3a17166e884a9ac4a715ded32e8d19c5
Thea House <theahouse10@gmail.com>
2019-07-25 12:24:12 +1000

    Download datasets

---
+++ Scripts/Data_Downloads.R
@@ -0,0 +1,3 @@
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.csv", destfile = "./data/SAFI_clean.csv")
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_dates.xlsx", destfile = "./data/SAFI_dates.xlsx", mode = "wb")
