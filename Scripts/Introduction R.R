69b2b81fde1ddec2e0afa24280d3d71c6db9bce6
Thea House <theahouse10@gmail.com>
2019-07-25 15:37:34 +1000

    Assign value to area_hectares.

---
+++ Scripts/Introduction R.R
@@ -0,0 +1,12 @@
+print ("Hello, world")
+library(tidyverse)
+library(tidyverse)      
+(area_hectares <- 1.0) # land area in hectares
+1.0 -> area_hectares
+
+area_hectares <- 2.5 # land area in hectares
+area_acres <- 2.47 * area_hectares # convert to acres
+area_acres #print
+# 
+3+5
+12/7
